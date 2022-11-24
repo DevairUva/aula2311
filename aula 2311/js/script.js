bf510a7baf1f90dac0f4a0de6c66fe0acaa04e97
DEVair Martins <83782674+DevairUva@users.noreply.github.com>
2022-11-23 21:26:45 -0300

    aula inteira

---
+++ aula 2311/js/script.js
@@ -0,0 +1,15 @@
+const frm = document.querySelector('form');
+const resp = document.querySelector('h2');
+frm.addEventListener('submit',(e)=>{
+    const name = frm.name.value;
+    const n1 = Number(frm.n1.value.replace(',','.'));
+    const n2 = Number(frm.n2.value.replace(',','.'));
+    const n3 = Number(frm.n3.value.replace(',','.'));
+    const n4 = Number(frm.n4.value.replace(',','.'));
+    let media = (n1+n2+n3+n4)/4;
+    media = media.toFixed(2);
+    media = media.toString();
+    media = media.replace('.',',');
+    resp.innerText = `${name}, sua média é: ${media}`;
+    e.preventDefault();
+})
